3db8e746cdce75df74cfa6d40cca2f2b2de87b6d
raghdaabassi <raghdaabassi@gmail.com>
2025-12-27 11:59:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,27 @@
 
 print("Hello, Git!")
+from netmiko import ConnectHandler
+
+device = {
+    "device_type": "cisco_ios",
+    "host": "sandbox-iosxr-1.cisco.com",
+    "username": "admin",
+    "password": "C1sco12345",
+    "port": 22,
+}
+
+def main():
+    connexion = ConnectHandler(**device)
+
+    # 1) show clock
+    print("=== DATE DU ROUTEUR ===")
+    print(connexion.send_command("show clock"))
+
+    # 2) Save interfaces
+    print("\n=== SAUVEGARDE DES INTERFACES ===")
+    interfaces = connexion.send_command("show ip interface brief")
+    with open("interfaces.txt", "w") as f:
+        f.write(interfaces)
+    print("Interfaces enregistrées dans interfaces.txt")
+if __name__ == "__main__":
+    main()
